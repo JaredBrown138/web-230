e0e959479ca338160d3ea17785c69617f7005436
Jared Brown <brownjt01@sunyjcc.edu>
2017-12-02 18:49:29 -0500

    Added the primitive-types exercise JS File

---
+++ week-1/primitive-types.js
@@ -0,0 +1,23 @@
+/*
+============================================
+; Title: Assignment 1.4
+; Authors: Professor Krasso, Jared Brown
+; Date: 2 December 2017
+; Description: A simple program that declares
+and assigns values to variable before printing
+them to the console.
+;===========================================
+*/ 
+
+var firstName = "Jared";
+var lastName = "Brown";
+var age = "20";
+var street = "34 Long Road";
+var city = "Jamestown";
+var state = "New York";
+var zipCode = "14701";
+
+var printString = firstName + "\n" + lastName + "\n" + age + 
+"\n" + street + "\n" + city + "\n" + state + "\n" + zipCode; 
+
+console.log(printString);

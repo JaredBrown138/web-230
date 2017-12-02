a7cc01a9ca7235533e23c80080b79b904aa65a29
Jared Brown <brownjt01@sunyjcc.edu>
2017-12-02 18:15:35 -0500

    Adding hello-world.js

---
+++ week-1/hello-world.js
@@ -0,0 +1,10 @@
+/*
+============================================
+; Title: Assignment 1.4
+; Authors: Professor Krasso, Jared Brown
+; Date: 2 December 2017
+; Description: A simple Hello World program 
+; for WEB 230
+;===========================================
+*/ 
+console.log("Hello World, my name is Jared Brown");

6a11c89a4014d0495d526e5594748dcca93de79c
Jared Brown <brownjt01@sunyjcc.edu>
2017-12-04 01:21:24 -0500

    Added the header.js and brown-exercise2-2.js files

---
+++ week-2/brown-exercise2-2.js
@@ -0,0 +1,15 @@
+/*
+============================================
+; Title:  Program Header
+; Author: Professor Krasso 
+; Date:   25 June 2017
+; Modified By: Jared Brown
+; Description: Displays a formatted header
+;===========================================
+*/ 
+var header = require('../header.js');
+
+console.log("\nTesting the header.js file's output..."); 
+
+console.log(header.display("Jared", "Brown", "Exercise 2.2")); 
+console.log("\n");
